da3f7b70f006fd78eff71c34fc0860d1f3f38c83
snhu-brett <brett.house@snhu.edu>
2026-04-19 17:43:15 -0700

    Add files via upload
    Improved files

---
+++ enhancements/databases/index.js
@@ -0,0 +1,53 @@
+const jwt = require('jsonwebtoken'); // Enable JSON Web Tokens
+const express = require("express");
+const router = express.Router();
+
+const tripsController = require("../controllers/trips");
+const authController = require("../controllers/authentication");
+
+// Method to authenticate our JWT
+function authenticateJWT(req, res, next) {
+  const authHeader = req.headers['authorization'];
+
+  if (authHeader == null) {
+    console.log('Auth Header Required but NOT PRESENT!');
+    return res.sendStatus(401);
+  }
+
+  let headers = authHeader.split(' ');
+  if (headers.length < 1) {
+    console.log('Not enough tokens in Auth Header: ' + headers.length);
+    return res.sendStatus(501);
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  if (token == null) {
+    console.log('Null Bearer Token');
+    return res.sendStatus(401);
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, verified) => {
+    if (err) {
+      return res.sendStatus(401).json('Token Validation Error!');
+    }
+    req.auth = verified;
+    next();
+  });
+}
+
+router.route("/register").post(authController.register);
+router.route("/login").post(authController.login);
+
+router
+  .route("/trips")
+  .get(tripsController.tripsList)
+  .post(authenticateJWT, tripsController.tripsAddTrip);
+
+router
+  .route("/trips/:tripCode")
+  .get(tripsController.tripsFindByCode)
+  .put(authenticateJWT, tripsController.tripsUpdateTrip)
+  .delete(authenticateJWT, tripsController.tripsDeleteTrip);
+
+module.exports = router;
